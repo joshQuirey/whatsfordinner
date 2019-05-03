d164e6f611089f56f7128af499d2940be54066bf
jquirey <jquirey@icloud.com>
2019-05-03 16:42:03 -0500

    Got the array to be loaded with a planned meal for each day

--- WhatsForDinner/View Controllers/CreatePlanViewController.swift
+++ WhatsForDinner/View Controllers/CreatePlanViewController.swift
@@ -115,17 +115,66 @@ class CreatePlanViewController: UIViewController, UIPickerViewDelegate, UIPicker
     }
     
     @IBAction func create(_ sender: Any) {
-        
-        //Day7
-        let day7Plan = PlannedDay(context: self.managedObjectContext!)
-        day7Plan.planStartDate = dateDay1
-        day7Plan.date = dateDay7
-        day7Plan.planEndDate = dateDay7
-        day7Plan.category = category7.text
+        //Day1
+        let day1Plan = PlannedDay(context: self.managedObjectContext!)
+        day1Plan.planStartDate = dateDay1
+        day1Plan.date = dateDay1
+        day1Plan.planEndDate = dateDay7
+        day1Plan.category = category1.text
         //Get Meal
-        day7Plan.meal = self.getMealforPlannedDay(_category: day7Plan.category!, _plannedDate: day7Plan.date!)
-        print("Selected Meal: \(day7Plan.meal!.mealName!)")
-        weekPlan.append(day7Plan)
+        day1Plan.meal = self.getMealforPlannedDay(_category: day1Plan.category!, _plannedDate: day1Plan.date!)
+        print("Selected Meal 1: \(day1Plan.meal!.mealName!)")
+        
+        weekPlan.append(day1Plan)
+        
+        //Day2
+        let day2Plan = PlannedDay(context: self.managedObjectContext!)
+        day2Plan.planStartDate = dateDay1
+        day2Plan.date = dateDay2
+        day2Plan.planEndDate = dateDay7
+        day2Plan.category = category2.text
+        //Get Meal
+        day2Plan.meal = self.getMealforPlannedDay(_category: day2Plan.category!, _plannedDate: day2Plan.date!)
+        print("Selected Meal 2: \(day2Plan.meal!.mealName!)")
+
+        weekPlan.append(day2Plan)
+        
+        //Day3
+        let day3Plan = PlannedDay(context: self.managedObjectContext!)
+        day3Plan.planStartDate = dateDay1
+        day3Plan.date = dateDay3
+        day3Plan.planEndDate = dateDay7
+               day3Plan.category = category3.text
+        //Get Meal
+        day3Plan.meal = self.getMealforPlannedDay(_category: day3Plan.category!, _plannedDate: day3Plan.date!)
+        print("Selected Meal 3: \(day3Plan.meal!.mealName!)")
+ 
+        weekPlan.append(day3Plan)
+        
+        //Day4
+        let day4Plan = PlannedDay(context: self.managedObjectContext!)
+        day4Plan.planStartDate = dateDay1
+        day4Plan.date = dateDay4
+        day4Plan.planEndDate = dateDay7
+                day4Plan.category = category4.text
+        //Get Meal
+        day4Plan.meal = self.getMealforPlannedDay(_category: day4Plan.category!, _plannedDate: day4Plan.date!)
+        print("Selected Meal 4: \(day4Plan.meal!.mealName!)")
+
+        weekPlan.append(day4Plan)
+
+        //Day5
+        let day5Plan = PlannedDay(context: self.managedObjectContext!)
+        day5Plan.planStartDate = dateDay1
+        day5Plan.date = dateDay5
+        day5Plan.planEndDate = dateDay7
+        day5Plan.category = category5.text
+        //Get Meal
+        day5Plan.meal = self.getMealforPlannedDay(_category: day5Plan.category!, _plannedDate: day5Plan.date!)
+        //if meal is null???
+        print("Selected Meal 5: \(day5Plan.meal!.mealName!)")
+        
+        weekPlan.append(day5Plan)
         
         //Day6
         let day6Plan = PlannedDay(context: self.managedObjectContext!)
@@ -135,68 +184,22 @@ class CreatePlanViewController: UIViewController, UIPickerViewDelegate, UIPicker
         day6Plan.category = category6.text
         //Get Meal
         day6Plan.meal = self.getMealforPlannedDay(_category: day6Plan.category!, _plannedDate: day6Plan.date!)
-        print("Selected Meal: \(day6Plan.meal!.mealName!)")
+        print("Selected Meal 6: \(day6Plan.meal!.mealName!)")
         weekPlan.append(day6Plan)
         
-        //Day5
-        let day5Plan = PlannedDay(context: self.managedObjectContext!)
-        day5Plan.planStartDate = dateDay1
-        day5Plan.date = dateDay5
-        day5Plan.planEndDate = dateDay7
+        //Day7
+        let day7Plan = PlannedDay(context: self.managedObjectContext!)
+        day7Plan.planStartDate = dateDay1
+        day7Plan.date = dateDay7
+        day7Plan.planEndDate = dateDay7
+        day7Plan.category = category7.text
         //Get Meal
-        day5Plan.meal = self.getMealforPlannedDay(_category: day5Plan.category!, _plannedDate: day5Plan.date!)
-        //if meal is null???
-        print("Selected Meal: \(day5Plan.meal!.mealName!)")
-        day5Plan.category = category5.text
+        day7Plan.meal = self.getMealforPlannedDay(_category: day7Plan.category!, _plannedDate: day7Plan.date!)
+        print("Selected Meal 7: \(day7Plan.meal!.mealName!)")
+        weekPlan.append(day7Plan)
         
-        weekPlan.append(day5Plan)
-        
-        //Day4
-        let day4Plan = PlannedDay(context: self.managedObjectContext!)
-        day4Plan.planStartDate = dateDay1
-        day4Plan.date = dateDay4
-        day4Plan.planEndDate = dateDay7
-        //Get Meal
-        day4Plan.meal = self.getMealforPlannedDay(_category: day4Plan.category!, _plannedDate: day4Plan.date!)
-        print("Selected Meal: \(day4Plan.meal!.mealName!)")
-        day4Plan.category = category4.text
-        weekPlan.append(day4Plan)
-        
-        //Day3
-        let day3Plan = PlannedDay(context: self.managedObjectContext!)
-        day3Plan.planStartDate = dateDay1
-        day3Plan.date = dateDay3
-        day3Plan.planEndDate = dateDay7
-        //Get Meal
-        day3Plan.meal = self.getMealforPlannedDay(_category: day3Plan.category!, _plannedDate: day3Plan.date!)
-        print("Selected Meal: \(day3Plan.meal!.mealName!)")
-        day3Plan.category = category3.text
-        weekPlan.append(day3Plan)
-        
-        //Day2
-        let day2Plan = PlannedDay(context: self.managedObjectContext!)
-        day2Plan.planStartDate = dateDay1
-        day2Plan.date = dateDay2
-        day2Plan.planEndDate = dateDay7
-        //Get Meal
-        day2Plan.meal = self.getMealforPlannedDay(_category: day2Plan.category!, _plannedDate: day2Plan.date!)
-        print("Selected Meal: \(day2Plan.meal!.mealName!)")
-        day2Plan.category = category2.text
-        weekPlan.append(day2Plan)
-        
-        //Day1
-        let day1Plan = PlannedDay(context: self.managedObjectContext!)
-        day1Plan.planStartDate = dateDay1
-        day1Plan.date = dateDay1
-        day1Plan.planEndDate = dateDay7
-        //Get Meal
-        day1Plan.meal = self.getMealforPlannedDay(_category: day1Plan.category!, _plannedDate: day1Plan.date!)
-        print("Selected Meal: \(day1Plan.meal!.mealName!)")
-        day1Plan.category = category1.text
-        weekPlan.append(day1Plan)
-        
-        //print(weekPlan)
-        
+        print("done done done")
+        print(weekPlan)
     }
     
     func getMealforPlannedDay(_category: String, _plannedDate: Date) -> Meal {
@@ -225,7 +228,25 @@ class CreatePlanViewController: UIViewController, UIPickerViewDelegate, UIPicker
                 //                }
                 
                 //check to see if meal is in current plan
-                _meal = meals[0]
+                if (weekPlan.count > 0) {
+                    var index: Int = 0
+                    //var keepSearching: Bool: true
+                    
+                    //while (keepSearching) {
+                        for day in weekPlan {
+                            //if meal already exists in current plan
+                            if (meals[index].objectID == day?.meal?.objectID) {
+                                index += 1
+                                break
+                            }
+                        }
+                    //}
+                    
+                    _meal = meals[index]
+                } else {
+                    _meal = meals[0]
+                }
+                
                 _meal.nextDate = _plannedDate
                 _meal.estimatedNextDate = nil
                 //when you mark complete prevdate becomes nextdate, nextdate becomes null, estimated gets calculated
